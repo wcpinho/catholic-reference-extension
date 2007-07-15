987a453d5c349ac6f7ac00b60418028d8dc9042b
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2007-07-15 21:29:43 +0000

    Plugin header.
    git-svn-id: http://localhost/svn/catholic-reference/trunk@1613 3bcaa56b-69e1-0310-823e-bac524374bd6

---
+++ plugin/catholic-reference.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Catholic Reference Extension for Wordpress
+Plugin URI: http://blog.purepistos.net
+Description: The Catholic Reference Extension makes scripture and Catechism references pop up the actual bible or Catechism text on hover.
+Version: 0.7.0
+Author: Pistos
+Author URI: http://blog.purepistos.net
+
+Copyright (c) 2007
+Released under the GPL license, version 2
+http://www.gnu.org/licenses/gpl.txt
+
+    This file is part of WordPress.
+    WordPress is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+
+?>
