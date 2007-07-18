05869e04ddc70c5f73a91a1736bb3880f778c88a
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2007-07-18 23:00:01 +0000

    Basic skeleton complete.  And it works!  :)
    git-svn-id: http://localhost/svn/catholic-reference/trunk@1615 3bcaa56b-69e1-0310-823e-bac524374bd6

--- plugin/catholic-reference.php
+++ plugin/catholic-reference.php
@@ -7,7 +7,7 @@ Version: 0.7.0
 Author: Pistos
 Author URI: http://blog.purepistos.net
 
-Copyright (c) 2007
+Copyright (c) 2007 Pistos
 Released under the GPL license, version 2
 http://www.gnu.org/licenses/gpl.txt
 
@@ -21,7 +21,16 @@ http://www.gnu.org/licenses/gpl.txt
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+function cathref_filter( $content ) {
+    return $content;
+}
+
+add_filter( 'the_content', 'cathref_filter' );
 
 ?>
