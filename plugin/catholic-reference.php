615151d8762046916ba0387e3c4ddbf6d6a50c29
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2007-07-19 19:03:00 +0000

    Next draft with prototype scripture substitution.
    git-svn-id: http://localhost/svn/catholic-reference/trunk@1617 3bcaa56b-69e1-0310-823e-bac524374bd6

--- plugin/catholic-reference.php
+++ plugin/catholic-reference.php
@@ -27,14 +27,47 @@ http://www.gnu.org/licenses/gpl.txt
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+$cathref_book_numbers = array(
+    'gen' => 1,
+    'genesis' => 1,
+    'exo' => 2,
+    'exod' => 2,
+    'exodus' => 2,
+);
+    
+function cathref_substitute_scripture( $matches ) {
+    $book = strtolower( $matches[ 1 ] );
+    $book_number = $cathref_book_numbers[ $book ];
+    $chapter = $matches[ 2 ];
+    if( $matches[ 3 ] ) {
+        $start_verse = $matches[ 3 ];
+        if( $matches[ 5 ] ) {
+            $verse_separator = $matches[ 4 ];
+            $end_verse = $matches[ 5 ];
+        } else {
+            $end_verse = $start_verse;
+        }
+    }
+    
+    $retval = "<span class=\"scripture\">BN $book_number $book $chapter";
+    if( $start_verse ) {
+        $retval .= " " . $start_verse;
+        if( $end_verse ) {
+            $retval .= $verse_separator . $end_verse;
+        }
+    }
+    $retval .= "</span>";
+    
+    return $retval;
+}
+
 function cathref_filter( $content ) {
     $drb_file = "/misc/pistos/unpack/douay-rheims.txt";
-    $book_numbers = array(
-        'gen' => 1,
-        'genesis' => 1,
-        'exo' => 2,
-        'exod' => 2,
-        'exodus' => 2,
+    
+    $content = preg_replace_callback(
+        "/((?:\\d+ +)?[A-Z][a-z]+)\\.? +(\\d+)(?: *: *(\\d+)(?: *(-|\\.{2,}) *(\\d+))?)?/",
+        'cathref_substitute_scripture',
+        $content
     );
 
     return $content;
