144ccb7bfe1f33f7bf4a0f31cecbb2512d596b31
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2007-07-22 18:30:47 +0000

    Began adding CCC.
    git-svn-id: http://localhost/svn/catholic-reference/trunk@1640 3bcaa56b-69e1-0310-823e-bac524374bd6

---
+++ convert-ccc.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+
+# Takes the downloaded CCC HTML files and extracts only the number paragraphs,
+# and places them into a few text files.
+# ./convert-ccc.rb <source directory> [output directory]
+
+require 'ostruct'
+require 'cgi'
+require 'hpricot'
+
+class String
+    def clean
+        CGI.unescapeHTML( gsub( /\s+/m, ' ' ) )
+    end
+end
+
+def write_para( para )
+    x = ( para / 100 ) * 100
+    filename = "#{$output_dir}/ccc-#{x+1}-#{x+100}.txt"
+    File.open( filename, 'a' ) do |f|
+        f.puts "<div class='cccp'>"
+        f.puts $paragraphs[ para ]
+        f.puts "</div>"
+    end
+end
+
+source_dir = ARGV[ 0 ] or exit
+$output_dir = ARGV[ 1 ] || '.'
+
+$paragraphs = Hash.new
+
+`rm #{$output_dir}/ccc*.txt`
+
+counter = 0
+para = nil
+Dir[ "#{source_dir}/__*" ].each do |filename|
+    puts filename
+    
+    active = false
+    doc = Hpricot( File.read( filename ) )
+    (doc/'p.MsoNormal').each do |p|
+        inner = p.inner_html
+        if inner =~ /^ *(\d+)/m
+            if para
+                write_para( para )
+            end
+            para = $1.to_i
+            $paragraphs[ para ] = inner.clean
+        elsif p[ :style ] == "margin-left:35.4pt"
+            if para
+                $paragraphs[ para ] << "\n\n" + inner.clean
+            else
+                puts "styled without leader:"
+                puts inner
+            end
+        else
+            #puts "** unknown"
+            #puts p.to_html
+            #puts "** end unknown"
+        end
+    end
+    counter += 1
+    break if counter >= 20
+end
+
+if para
+    write_para( para )
+end
