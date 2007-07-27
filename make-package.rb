4c003694581339a5622e607f1374dffca1d262d5
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2007-07-27 19:00:49 +0000

    Added make-package.rb.
    git-svn-id: http://localhost/svn/catholic-reference/trunk@1671 3bcaa56b-69e1-0310-823e-bac524374bd6

---
+++ make-package.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# Makes packages for the CRE.
+
+def run( command )
+    puts command
+    puts `#{command}`
+end
+
+work_dir = ARGV[ 1 ] || ( ENV[ 'HOME' ] + "/tmp" )
+Dir.chdir( work_dir ) do
+    if ARGV[ 0 ]
+        version = "tags/#{ARGV[ 0 ]}"
+    else
+        version = "trunk"
+    end
+    
+    run "rm -rf catholic-reference"
+    run "rm -ir catholic-reference*"
+    run "svn export http://rome.purepistos.net/svn/catholic-reference/#{version}/plugin catholic-reference"
+    run "zip -r -9 catholic-reference-#{version}.zip catholic-reference/"
+    run "tar cjvf catholic-reference-#{version}.tar.bz2 catholic-reference/"
+end
