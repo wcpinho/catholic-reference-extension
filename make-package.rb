2d2ce7b5a124074a166ad375e10d7e9b48645b5b
pistos <pistos@3bcaa56b-69e1-0310-823e-bac524374bd6>
2007-08-02 03:07:07 +0000

    Fixed versioned package making.
    git-svn-id: http://localhost/svn/catholic-reference/trunk@1708 3bcaa56b-69e1-0310-823e-bac524374bd6

--- make-package.rb
+++ make-package.rb
@@ -10,17 +10,18 @@ end
 work_dir = ARGV[ 1 ] || ( ENV[ 'HOME' ] + "/tmp" )
 Dir.chdir( work_dir ) do
     if ARGV[ 0 ]
-        version = "tags/#{ARGV[ 0 ]}"
+        version = ARGV[ 0 ]
+        svn_dir = "tags/#{version}"
     else
-        version = "trunk"
+        svn_dir = version = "trunk"
     end
     
     run "rm -rf catholic-reference"
     run "rm -ir catholic-reference*"
-    run "svn export http://rome.purepistos.net/svn/catholic-reference/#{version}/plugin catholic-reference"
+    run "svn export http://rome.purepistos.net/svn/catholic-reference/#{svn_dir}/plugin catholic-reference"
     run "zip -r -9 catholic-reference-#{version}.zip catholic-reference/"
     run "tar cjvf catholic-reference-#{version}.tar.bz2 catholic-reference/"
-    run "svn export http://rome.purepistos.net/svn/catholic-reference/#{version}/texts catholic-reference/texts"
+    run "svn export http://rome.purepistos.net/svn/catholic-reference/#{svn_dir}/texts catholic-reference/texts"
     run "zip -r -9 catholic-reference-#{version}-full.zip catholic-reference/ "
     run "tar cjvf catholic-reference-#{version}-full.tar.bz2 catholic-reference/"
 end
