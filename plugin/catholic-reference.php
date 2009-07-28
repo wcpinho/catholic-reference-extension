71ef4704a4f1043ffb0bf7e5f19bd39067c6c9db
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-07-28 12:53:48 -0400

    Changed licence.  Updated copyright years.

--- plugin/catholic-reference.php
+++ plugin/catholic-reference.php
@@ -29,24 +29,10 @@ CCC paragraphs 1234,1237
 
 --------------------------------------
 
-Copyright (c) 2007 Pistos
-Released under the GPL license, version 2
-http://www.gnu.org/licenses/gpl.txt
+Copyright (c) 2007-2009 Pistos
+Released under the MIT license
+http://www.opensource.org/licenses/mit-license.php
 
-    This file is part of WordPress.
-    WordPress is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 function cathref_initialize() {
